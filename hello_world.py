f023e5c840e5c345a181e23de27f25e9ee6f0213
justine thomas <justinethomas009@gmail.com>
2018-06-06 21:06:52 +0530

    added hello_world

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -----------------------------------------------------------------------------
+# Created: Wed  6 Jun 2018 21:05:54 IST
+# Last-Updated: Wed  6 Jun 2018 21:06:31 IST
+#
+# hello_world.py is part of travis-lab
+# URL: https://github.com/justinethomas009/travis-lab
+# Description:
+#
+# Copyright (c) 2018, Justine T Kizhakkinedath
+# All rights reserved
+#
+# LICENSE file not available
+# -----------------------------------------------------------------------------
+print("Hello world!")
